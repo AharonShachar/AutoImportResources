cb17214cf47f3b8e746f74edc0fc724f00cc3490
AharonShachar <aharon.s@qualisystems.com>
2016-03-08 23:41:13 +0200

    Update AutoImportReources.py

--- AutoImportReources.py
+++ AutoImportReources.py
@@ -2,51 +2,62 @@ __author__ = 'Aharon.S'
 import os
 import csv
 from qualipy.api.cloudshell_api import *
-import qualipy.api.cloudshell_api as API
-import qualipy.scripts.cloudshell_dev_helpers as dev
-import qualipy.scripts.cloudshell_scripts_helpers as script_helper
+import HTMLParser
 import  xml.etree.ElementTree as ET
-from xml.sax.saxutils import unescape
+
+
 
 #Globals
 AttrbuteIndexInCSVFile = 7
 FamilyColumn = 3
 ModelColumn = 4
 ResourceNameColumn = 2
+ParentColumn = 0
 
 class CloudShellManager:
     def __init__(self, session):
+        self.ns = {'xsd': 'http://www.w3.org/2001/XMLSchema',
+              'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+              'xmlns': 'http://schemas.qualisystems.com/ResourceManagement/ExportImportConfigurationSchema.xsd'}
         self.session = session
         self.Families = []
         self.models = []
         self.attributes = []
-        filename = "C:\\Users\\aharon.s\\Desktop\\system.xml"
-        file_object = open(filename, "r")
-        self.Tree = ET.fromstring( file_object.read())
-        #xmlres = session.ExportFamiliesAndModels()
-        #unescape. xmlres
-        #file_object.write(xmlres)
-        #xmlres = xmlres.replace("&gt;", ">")
-        #xmlres = xmlres.replace("&gt;&#xD;", ">")
-        #xmlres = xmlres.replace("&lt;", "<")
-        #xmlres = xmlres.replace("&#xD;", "")
-        #xmlres = xmlres.replace('xmlns="[^"]+"', '', 1000)
-        #res1 = xmlres.replace("xmlns:xsd=""http://www.w3.org/2001/XMLSchema""", "", 1000)
-        #xmlres = xmlres.replace("xmlns=""http://schemas.qualisystems.com/ResourceManagement/ExportImportConfigurationSchema.xsd""", "")
+
+        #filename = "C:\\Users\\aharon.s\\Desktop\\system.xml"
+        #file_object = open(filename, "r")
+        #self.Tree = ET.fromstring( file_object.read())
+
+        xmlres = session.ExportFamiliesAndModels()
+        xmlres = HTMLParser.HTMLParser().unescape(xmlres)
+
+        appendixNnodelocation = xmlres.index('<ResourceManagementExportImport')
+        xmlres = xmlres.replace(xmlres[0:appendixNnodelocation],'',appendixNnodelocation)
+
+        suffixNnodelocation = xmlres.index('</Configuration></ResponseInfo></Response>')
+        xmlres = xmlres.replace(xmlres[suffixNnodelocation: len(xmlres)],'',len(xmlres))
+
+        self.Tree = ET.fromstring(xmlres)
 
     def IslegalResource(self, row):
         res = True
 
         familyName = row[FamilyColumn]
         modelName = row[ModelColumn]
-        perdicate = 'ResourceFamilies/ResourceFamily[@Name="_family_name_"]/Models/ResourceModel[@Name="_model_name_"]'
+        name = row[ResourceNameColumn]
+
+        if (not familyName or not modelName or familyName.isspace() or modelName.isspace() ):
+            print ("Error create resource: "+ row[ResourceNameColumn]+ ". The Model / Familiy can't be empty")
+            return False
+        if (name.isspace()):
+            print ("Error create resource: "+ row[ResourceNameColumn]+ ". resource name can't be empty")
+            return False
+
+
+        perdicate = 'xmlns:ResourceFamilies/xmlns:ResourceFamily[@Name="_family_name_"]/xmlns:Models/xmlns:ResourceModel[@Name="_model_name_"]'
         perdicate = perdicate.replace('_family_name_', familyName)
         perdicate = perdicate.replace('_model_name_', modelName)
-        model = self.Tree.findall(perdicate)
-
-        if len(model) == 0:
-            print ("Can't locate Family/ Model: " + row[FamilyColumn] + '/' + row[ModelColumn] + " for the following resource: " + row[ResourceNameColumn])
-            res = False
+        model = self.Tree.findall(perdicate,self.ns)
 
         return res
 
@@ -56,32 +67,36 @@ class CloudShellManager:
         familyName = row[FamilyColumn]
         modelName = row[ModelColumn]
 
-        perdicate = '../ResourceFamilies/ResourceFamily[@Name="_family_name_"]/AttachedAttributes/AttachedAttribute'
-        perdicate = perdicate.replace('_family_name_', familyName)
-        familyAttributes = self.Tree.findall(perdicate)
 
-        perdicate = 'ResourceFamilies/ResourceFamily[@Name="_family_name_"]/Models/ResourceModel[@Name="_model_name_"]//AttachedAttribute'
+        if (not familyName or not modelName or familyName.isspace() or modelName.isspace() ):
+            print ("Error set addtibute for resource: "+ row[ResourceNameColumn]+ ". The Model / Familiy can't be empty")
+            return False
+
+        perdicate = 'xmlns:ResourceFamilies/xmlns:ResourceFamily[@Name="_family_name_"]/xmlns:Models/xmlns:ResourceModel[@Name="_model_name_"]/xmlns:AttributeValues'
         perdicate = perdicate.replace('_family_name_', familyName)
         perdicate = perdicate.replace('_model_name_', modelName)
-        modelAttributes = self.Tree.findall(perdicate)
+        modelAttributes = self.Tree.findall(perdicate, self.ns)
+        attributesModelKeys = []
 
-        legalAttributes = familyAttributes + modelAttributes
-        resourceAttributrs = row[AttrbuteIndexInCSVFile:len(row)]
-
-        filterdResourceAttributrs = filter(None,resourceAttributrs)
-        if not filterdResourceAttributrs: #no attributes exists
+        if not modelAttributes:
+            print ("No defined attributes for resource Family/ Model: '" + familyName + "'/ '" + modelName +"'" )
             return False
+
+        for modelAttribute in modelAttributes[0]._children:
+            attributesModelKeys.append(modelAttribute.attrib['Name'])
+
+        #List of all the resource attributes
+        resourceAttributrs = row[AttrbuteIndexInCSVFile:len(row)]
 
         for index, attribute in enumerate(resourceAttributrs):
             if attribute:
                 attributeKey = self.attributes[index]
-                if not attributeKey in legalAttributes:
-                    print ("Can't locate attribute: " + attributeKey + " for the following resource: " + row[ResourceNameColumn])
+                if not attributeKey in attributesModelKeys:
+                    print ("Can't locate attribute: '" + attributeKey + "' for the following resource: " + row[ResourceNameColumn])
                     return False
-
         return res
 
-    def AddRow(self, row):
+    def AddResource(self, row):
         parent = row[0]
         description = row[1]
         name = row[2]
@@ -89,58 +104,94 @@ class CloudShellManager:
         model = row[4]
         folder = row[5]
         address = row[6]
-        if name == 'Name':#Row 0 - save the attributes keys
-            if len(row) > AttrbuteIndexInCSVFile:
-                self.attributes = row[AttrbuteIndexInCSVFile:len(row)-1]
-        else:
-            if folder and (not parent):
-                self.session.CreateFolder(folder)
 
-            try:#add reource
-                if self.IslegalResource(row):
-                    res = self.session.CreateResource(family, model, name, address, folder, parent, description)
-                    print('Resource: '+name+'added successfully.' )
+        try:#add reource
+            if self.IslegalResource(row):
+                if(parent):
+                    folder = self.session.GetResourceDetails(parent).FolderFullPath
 
-            except CloudShellAPIError as error:
-                print 'Error creating resource: '+name+'. '+error.message
+            res = self.session.CreateResource(family, model, name, address, folder, parent, description)
+            print('Resource: '+name+'added successfully.' )
 
-            try:#add reesource attributes
-                if self.IsLegalResourceAttributes(row):
-                    self.SetAttributes(row)
-                    print('Resource attributes : '+name+'added successfully.' )
+        except CloudShellAPIError as error:
+            print 'Error creating resource: '+name+'. '+error.message
 
-            except CloudShellAPIError as error:
-                print 'Error adding resource attributes: '+name+'. '+error.message
+    def AddAttributes(self, row):
 
+        name = row[ResourceNameColumn]
+
+        if not len(row) <= AttrbuteIndexInCSVFile:
+            attributes = row[AttrbuteIndexInCSVFile:len(row)]
+
+        isEmptyList = True
+
+        for attribute in attributes:
+            if attribute and not attribute.isspace():#there is real attribute
+                isEmptyList = False
+                break
+
+        if isEmptyList:
+            print('No attributes for resource: '+ row[ResourceNameColumn])
+            return
+
+        try:#add reesource attributes
+            if self.IsLegalResourceAttributes(row):
+                self.SetAttributes(row)
+                print('Attributes for resource: '+ name +' added successfully.' )
+
+        except CloudShellAPIError as error:
+            print 'Error adding resource attributes: '+name+'. '+error.message
 
     def SetAttributes(self, row):
+
         if not len(row) <= AttrbuteIndexInCSVFile:
-            attributes = row[AttrbuteIndexInCSVFile:len(row)-1]
+            attributes = row[AttrbuteIndexInCSVFile:len(row)]
+
+            resourceName = row[ResourceNameColumn]
+
             for index,  att in enumerate(attributes):
+                name = row[ResourceNameColumn]
+                parent = row[ParentColumn]
 
-                name = row[2]
-                parent = row[0]
-
-                if parent:  # Not root resource
-                    fullName = row[0] + '\\' + name
+                if parent and not parent.isspace():  # Not root resource
+                    fullName = row[ParentColumn] + '\\' + name
                 else:
                     fullName = name
 
                 AttributeName = self.attributes[index]
                 AttributeValue = attributes[index]
-                if not AttributeValue:
+
+                if AttributeValue and not AttributeValue.ispace():
                     self.session.SetAttributeValue(fullName, AttributeName, AttributeValue)
+                    print ("Attribute set - Attribute name: " + AttributeValue + ", Attribute value: "+AttributeValue +", for resource: " + resourceName  )
 
 
 ApiSession = CloudShellAPISession("localhost", "admin", "admin", "Global")
 manager = CloudShellManager(ApiSession)
-#ApiSession.SetAttributesValues()
+
 
 with open('C:\\Users\\aharon.s\\Dropbox\\\QualiSystems\\Python\\Resources.csv') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='\n')
     for row in spamreader:
+
+        parent = row[0]
+        description = row[1]
+        name = row[2]
+        family = row[3]
+        model = row[4]
+        folder = row[5]
+        address = row[6]
+
         try:
-            manager.AddRow(row)
+            if name == 'Name':#Row 0 -
+                if len(row) > AttrbuteIndexInCSVFile:
+                   manager.attributes = row[AttrbuteIndexInCSVFile:len(row)]#save the attributes keys
+            else:
+                if folder and (not parent):
+                    manager.session.CreateFolder(folder)
+
+                manager.AddResource(row)
+                manager.AddAttributes(row)
 
         except CloudShellAPIError as error:
             print error.message + ". Input entry row in CSV: " + ', '.join(row)
